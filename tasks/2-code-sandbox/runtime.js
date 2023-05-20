81fdd9e2af7504e0201509facbd59a5fbbf0a51f
Walter Banda <walterkaimurima@gmail.com>
2023-05-20 21:32:18 +0300

    ☕ Initial reference of DOM components

---
+++ tasks/2-code-sandbox/runtime.js
@@ -0,0 +1,14 @@
+/**
+ * DOM Components references
+ */
+
+const code_textbox = document.getElementById();
+const code_selector = document.getElementById();
+
+const output_container = document.getElementById();
+const download_outputBtn = document.getElementById();
+const logs_container = document.getElementById();
+
+const yagna_appkeytext = document.getElementById();
+const node_selector = document.getElementById();
+const run_btn = document.getElementById();
