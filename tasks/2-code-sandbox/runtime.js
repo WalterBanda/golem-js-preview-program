1759d57d80c90ffc49499222f91f1d0e4b3e100d
Walter Banda <walterkaimurima@gmail.com>
2023-05-21 11:35:58 +0300

    ☕ DOM references

--- tasks/2-code-sandbox/runtime.js
+++ tasks/2-code-sandbox/runtime.js
@@ -2,13 +2,13 @@
  * DOM Components references
  */
 
-const code_textbox = document.getElementById();
-const code_selector = document.getElementById();
+const code_textbox = document.getElementById('code');
+const code_selector = document.getElementById('runtime');
 
-const output_container = document.getElementById();
-const download_outputBtn = document.getElementById();
-const logs_container = document.getElementById();
+const output_container = document.getElementById('output');
+const download_outputBtn = document.getElementById('outputBtn');
+const logs_container = document.getElementById('logs');
 
-const yagna_appkeytext = document.getElementById();
-const node_selector = document.getElementById();
-const run_btn = document.getElementById();
+const yagna_appkeytext = document.getElementById('appid');
+const node_selector = document.getElementById('node');
+const run_btn = document.getElementById('run');
