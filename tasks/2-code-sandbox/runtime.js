d3977327fe70abe3ff84405861573d386e19f13f
Walter Banda <walterkaimurima@gmail.com>
2023-05-21 11:48:36 +0300

    ☕  Action funtions + Exports of references and utiliy fn

--- tasks/2-code-sandbox/runtime.js
+++ tasks/2-code-sandbox/runtime.js
@@ -3,12 +3,28 @@
  */
 
 const code_textbox = document.getElementById('code');
-const code_selector = document.getElementById('runtime');
+const yagna_appkeytext = document.getElementById('appid');
 
 const output_container = document.getElementById('output');
-const download_outputBtn = document.getElementById('outputBtn');
 const logs_container = document.getElementById('logs');
 
-const yagna_appkeytext = document.getElementById('appid');
-const node_selector = document.getElementById('node');
-const run_btn = document.getElementById('run');
+
+function runSandbox() {
+
+}
+
+function getRuntime(value) {
+
+}
+
+function getSandbox(value) {
+
+}
+
+function downloadOutput() {
+
+}
+
+module.exports = {
+    yagna_appkeytext, code_textbox, output_container, logs_container
+}
